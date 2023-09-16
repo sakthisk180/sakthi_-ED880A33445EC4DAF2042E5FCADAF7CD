7c8bead309045752eca75f6abb73e943e75c25cb
sakthisk180 <145181390+sakthisk180@users.noreply.github.com>
2023-09-15 22:06:45 -0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+#Implement a recursive function to calculate the factortal of a given number.
+
+
+def fact(n):
+  if n == 0 or n ==1:
+    return 1
+  else:
+    return n * fact(n-1)
+
+
+number = int(input("Enter a value:"))
+
+res = fact(number)
+print("The factorial of {} is {}.".format(number, res))
